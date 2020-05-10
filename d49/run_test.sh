2804d3f92a74d012eb9aa29fe3c07c59155abb8b
jianhaosu <jianhaosu>
2020-05-10 23:39:15 +0800

    final

---
+++ d49/run_test.sh
@@ -0,0 +1,2 @@
+python yolo_video.py --input=final/Kangaroo.mp4 --output=final/kangaroo.out.mp4 --classes=model_data/cvml_classes.txt --model=logs/2020/trained_weights_final.h5
+#python yolo_video.py --input=final/Raccoon.mp4 --output=final/Raccoon.out.mp4 --classes_path=model_data/cvml_classes.txt --model_path=logs/2020/trained_weights_final.h5
